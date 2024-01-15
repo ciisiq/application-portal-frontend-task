fa5e80f0d6efb5cee4bf60162672fb302accda04
ciisiq <cii_3@hotmail.com>
2024-01-15 16:10:52 +0000

    test: making some test, see more on comments.md

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, getByText, render, screen } from '@testing-library/react';
 import App from '../app/App';
+import { getApplications } from '../api/applicationsServices';
+import { describe, test, expect, vi } from 'vitest';
 
 test('renders "Application Portal" title', () => {
   render(<App />);
   const linkElement = screen.getByText(/Application portal/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe('Check the application client side', () => {
+  it('fetches successfully data from an API', async () => {
+    const mockData = [{ id: 1, name: 'Application 1' }];
+    global.fetch = vi.fn().mockResolvedValue({
+      json: vi.fn().mockResolvedValue(mockData),
+    });
+    const result = await getApplications(1, 5);
+    expect(result).toEqual(mockData);
+  });
+
+  // I was folling reference from the  vitest docs https://vitest.dev/api/#test-fails
+  // I couldn't make this work
+  // it('will no fetch data from an API', async () => {
+  //   await expect(getApplications(1, 5)).rejects.toThrowError('Network Error');
+  // });
 });
